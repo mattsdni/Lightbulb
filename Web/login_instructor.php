0b5f8302a13c5b816715901f98fce083d30877c6
Matt <mattsdni@users.noreply.github.com>
2016-02-21 23:09:06 -0800

    allows auto login for hackathon

---
+++ Web/login_instructor.php
@@ -0,0 +1,25 @@
+<?php
+
+    // configuration
+    require("../includes/config.php");
+
+        // query database for user
+        $rows = query("SELECT * FROM users WHERE email = ?", "teacher@school.edu");
+
+        // if we found user, check password
+        if (count($rows) == 1)
+        {
+            // first (and only) row
+            $row = $rows[0];
+
+            // remember that user's now logged in by storing user's ID in session
+            $_SESSION["id"] = $row["id"];
+            $_SESSION["type"] = $row["type"];
+            $_SESSION["name"] = $row["name"];
+
+            // redirect to home
+            redirect("/");
+        }
+
+
+?>
